276686c4fa91197aeb96882f46c1a26686b4352d
Davi Mendes <95036138+davijonm@users.noreply.github.com>
2024-01-14 13:07:11 -0300

    Update urls.py

--- backend/django_react/urls.py
+++ backend/django_react/urls.py
@@ -21,6 +21,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
-
-# Qualquer URL que não seja as setadas acima, serão apontadas para o react
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
